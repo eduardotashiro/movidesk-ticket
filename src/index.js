230828acfc9dbe7abc4b969c8f9399025484bec3
Eduardo Tashiro <eduardogaia854@hotmail.com>
2025-10-13 11:22:04 -0300

    chore: update server start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { app } from "./app.js"
 
+
+
 (async () => {
     await app.start(process.env.PORT || 3000)
-    console.log(`bot rodando na porta ${process.env.PORT || 3000}!`)
+    console.log(`Tickets batendo na porta ${process.env.PORT || 3000}!`)
 })()
+
